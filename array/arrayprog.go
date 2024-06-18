adb9daa5c50846b6f952d6d1c962aa13ba1846aa
Aniruddha Sinha <asinha0493@gmail.com>
2024-06-19 01:20:18 +0530

    Added array problems

---
+++ array/arrayprog.go
@@ -0,0 +1,11 @@
+package array
+
+//the questions for array have been acquired from
+//https://www.includehelp.com/golang/array-programs.aspx
+
+func DemoArray() {
+	var names []string
+	names[0] = "ford"
+	names[1] = "porsche"
+
+}
